f56de3e2af0605fbfd71928d9cf943c0117c9b33
Vinicius Nakamura Goes <vininha.goes@gmail.com>
2024-12-02 22:53:14 +0900

    first commit

--- app/Http/Services/BaseService.php
+++ app/Http/Services/BaseService.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Services;
-use App\Http\Repositories\BaseRepository;
+use App\Repositories\BaseRepository;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
---
+++ app/Models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
+
+    protected $table = 'users';
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
