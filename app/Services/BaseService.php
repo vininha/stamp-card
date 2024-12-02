e32bcc1e81640951429d8dcc3ec58e0efc89f81a
Vinicius Nakamura Goes <vininha.goes@gmail.com>
2024-12-02 22:56:15 +0900

    move service folder to app root

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Services;
+namespace App\Services;
 use App\Repositories\BaseRepository;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
